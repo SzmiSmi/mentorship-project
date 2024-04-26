bf192a53f2950998a559d1d63237cd3511db4cc3
SzmiSmi <97198979+SzmiSmi@users.noreply.github.com>
2024-04-26 17:01:16 -0400

    configuring cpp connect

--- docker-mysql/src/main.cpp
+++ docker-mysql/src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <../mysql-connector-c++-8.3.0-macos14-arm64/include/mysqlx/xdevapi.h>
+
 using namespace std;
+using ::std::endl;
+using namespace ::mysqlx;
+
 
 int main () {
     std::cout << "Hello World!";
